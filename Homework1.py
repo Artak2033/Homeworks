32e360d2535fc931da598bab6475d38816eb22a3
Artak2033 <akfartak@mail.ru>
2021-05-10 12:08:06 +0400

    First

---
+++ Homework1.py
@@ -0,0 +1,12 @@
+# 4. Write a Python program use input() to print Hello (your name) , I am (your
+# age) years old.
+
+name = input('Your name: ')
+age = input('Your age: ')
+
+print('Hello ' + name + ', I am ' + age + ' years old.')
+
+
+# 5.Write a Python program to print the following string
+
+# print('Python is powerful... and fast;\n\tplays well with others;\nruns everywhere;is friendly & easy to learn')
